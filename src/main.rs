33e9d2a5de19176596f6b91f49b6935bd4158754
Molly Novash <mnovash@yahoo.com>
2019-06-04 10:37:09 -0700

    working on hardware stuff

--- src/main.rs
+++ src/main.rs
@@ -31,9 +31,26 @@ fn test_rppal() -> Result <(), Box<dyn Error>> {
   Ok(())
 }
 
-fn main() {
+fn main() -> Result <(), Box<dyn Error>> {
   // initialize hardware
-  init_hardware();
+  let pinout: Pinout = init_pinout();
+
+  let mut gpio = GPIO { r1_gpio: Gpio::new()?.get(pinout.r1_GPIO)?.into_output(),
+                        r2_gpio: Gpio::new()?.get(pinout.r2_GPIO)?.into_output(),
+                        r3_gpio: Gpio::new()?.get(pinout.r3_GPIO)?.into_output(),
+                        r4_gpio: Gpio::new()?.get(pinout.r4_GPIO)?.into_output(),
+                        y1_gpio: Gpio::new()?.get(pinout.y1_GPIO)?.into_output(),
+                        y2_gpio: Gpio::new()?.get(pinout.y2_GPIO)?.into_output(),
+                        y3_gpio: Gpio::new()?.get(pinout.y3_GPIO)?.into_output(),
+                        y4_gpio: Gpio::new()?.get(pinout.y4_GPIO)?.into_output(),
+                        g1_gpio: Gpio::new()?.get(pinout.g1_GPIO)?.into_output(),
+                        g2_gpio: Gpio::new()?.get(pinout.g2_GPIO)?.into_output(),
+                        g3_gpio: Gpio::new()?.get(pinout.g3_GPIO)?.into_output(),
+                        g4_gpio: Gpio::new()?.get(pinout.g4_GPIO)?.into_output(),
+                        b1_gpio: Gpio::new()?.get(pinout.b1_GPIO)?.into_output(),
+                        b2_gpio: Gpio::new()?.get(pinout.b2_GPIO)?.into_output(),
+                        b3_gpio: Gpio::new()?.get(pinout.b3_GPIO)?.into_output(),
+                        b4_gpio: Gpio::new()?.get(pinout.b4_GPIO)?.into_output() };
 
   // use 'hound' crate to read in a given .wav file
   let mut reader = hound::WavReader::open("wav_files/ImIntoYou.wav").unwrap();
@@ -54,37 +71,101 @@ fn main() {
     let temp_vec = chunk.to_vec();
     if let Some(peak) = find_peak(temp_vec) {
       // calculate the range of the peak
-      pick_color(peak);
+      pick_color(peak, gpio);
     }
+  }
+  Ok(())
+}
+
+struct Pinout {
+  r1_GPIO: u8,
+  r2_GPIO: u8,
+  r3_GPIO: u8,
+  r4_GPIO: u8,
+  y1_GPIO: u8,
+  y2_GPIO: u8,
+  y3_GPIO: u8,
+  y4_GPIO: u8,
+  g1_GPIO: u8,
+  g2_GPIO: u8,
+  g3_GPIO: u8,
+  g4_GPIO: u8,
+  b1_GPIO: u8,
+  b2_GPIO: u8,
+  b3_GPIO: u8,
+  b4_GPIO: u8,
+}
+
+#[derive(Debug, Copy, Clone)]
+struct GPIO {
+  r1_gpio: rppal::gpio::OutputPin,
+  r2_gpio: rppal::gpio::OutputPin,
+  r3_gpio: rppal::gpio::OutputPin,
+  r4_gpio: rppal::gpio::OutputPin,
+  y1_gpio: rppal::gpio::OutputPin,
+  y2_gpio: rppal::gpio::OutputPin,
+  y3_gpio: rppal::gpio::OutputPin,
+  y4_gpio: rppal::gpio::OutputPin,
+  g1_gpio: rppal::gpio::OutputPin,
+  g2_gpio: rppal::gpio::OutputPin,
+  g3_gpio: rppal::gpio::OutputPin,
+  g4_gpio: rppal::gpio::OutputPin,
+  b1_gpio: rppal::gpio::OutputPin,
+  b2_gpio: rppal::gpio::OutputPin,
+  b3_gpio: rppal::gpio::OutputPin,
+  b4_gpio: rppal::gpio::OutputPin,
+}
+
+impl GPIO {
+  fn green_on(&mut self) {
+    self.g1_gpio.set_high();
+    thread::sleep(Duration::from_millis(250));
+    self.g1_gpio.set_low();
+    self.g2_gpio.set_high();
+    thread::sleep(Duration::from_millis(250));
+    self.g2_gpio.set_low();
+    self.g3_gpio.set_high();
+    thread::sleep(Duration::from_millis(250));
+    self.g3_gpio.set_low();
+    self.g4_gpio.set_high();
+    thread::sleep(Duration::from_millis(250));
+    self.g4_gpio.set_low();
   }
 }
 
-fn init_hardware() -> Result <(), Box<dyn Error>> {
+fn init_pinout() -> Pinout {
   // set GPIO pins values for each color as global constants
   // note that "gpio" from rppal uses BCM pin numbering
+  let pinout: Pinout = Pinout { r1_GPIO :  2, r2_GPIO :  3, r3_GPIO :  4, r4_GPIO : 17,
+                                y1_GPIO : 27, y2_GPIO : 22, y3_GPIO : 10, y4_GPIO :  9,
+                                g1_GPIO :  5, g2_GPIO :  6, g3_GPIO : 13, g4_GPIO : 19,
+                                b1_GPIO : 12, b2_GPIO : 16, b3_GPIO : 20, b4_GPIO : 21 } ;
 
-  let r1_GPIO: u8 = 2;
-  let r2_GPIO: u8 = 3;
-  let r3_GPIO: u8 = 4;
-  let r4_GPIO: u8 = 17;
-
-  let y1_GPIO: u8 = 27;
-  let y2_GPIO: u8 = 22;
-  let y3_GPIO: u8 = 10;
-  let y4_GPIO: u8 = 9;
-
-  let g1_GPIO: u8 = 5;
-  let g2_GPIO: u8 = 6;
-  let g3_GPIO: u8 = 13;
-  let g4_GPIO: u8 = 19;
-
-  let b1_GPIO: u8 = 12;
-  let b2_GPIO: u8 = 16;
-  let b3_GPIO: u8 = 20;
-  let b4_GPIO: u8 = 21;
+  pinout
 
   // declare the 16 GPIO pins and set their modes to "output"
+/*
+  let mut gpio = GPIO { r1_gpio: Gpio::new()?.get(pinout.r1_GPIO)?.into_output(),
+                        r2_gpio: Gpio::new()?.get(pinout.r2_GPIO)?.into_output(),
+                        r3_gpio: Gpio::new()?.get(pinout.r3_GPIO)?.into_output(),
+                        r4_gpio: Gpio::new()?.get(pinout.r4_GPIO)?.into_output(),
+                        y1_gpio: Gpio::new()?.get(pinout.y1_GPIO)?.into_output(),
+                        y2_gpio: Gpio::new()?.get(pinout.y2_GPIO)?.into_output(),
+                        y3_gpio: Gpio::new()?.get(pinout.y3_GPIO)?.into_output(),
+                        y4_gpio: Gpio::new()?.get(pinout.y4_GPIO)?.into_output(),
+                        g1_gpio: Gpio::new()?.get(pinout.g1_GPIO)?.into_output(),
+                        g2_gpio: Gpio::new()?.get(pinout.g2_GPIO)?.into_output(),
+                        g3_gpio: Gpio::new()?.get(pinout.g3_GPIO)?.into_output(),
+                        g4_gpio: Gpio::new()?.get(pinout.g4_GPIO)?.into_output(),
+                        b1_gpio: Gpio::new()?.get(pinout.b1_GPIO)?.into_output(),
+                        b2_gpio: Gpio::new()?.get(pinout.b2_GPIO)?.into_output(),
+                        b3_gpio: Gpio::new()?.get(pinout.b3_GPIO)?.into_output(),
+                        b4_gpio: Gpio::new()?.get(pinout.b4_GPIO)?.into_output() };
+
+*/
+ /*
   let mut r1_gpio = Gpio::new()?.get(r1_GPIO)?.into_output();
+  let () = r1_gpio;
   let mut r2_gpio = Gpio::new()?.get(r2_GPIO)?.into_output();
   let mut r3_gpio = Gpio::new()?.get(r3_GPIO)?.into_output();
   let mut r4_gpio = Gpio::new()?.get(r4_GPIO)?.into_output();
@@ -105,9 +186,11 @@ fn init_hardware() -> Result <(), Box<dyn Error>> {
   let mut b4_gpio = Gpio::new()?.get(b4_GPIO)?.into_output();
 
   Ok(())
+*/
+  //return (Ok(()), gpio);
 }
 
-fn pick_color(frequency: f32) -> () {
+fn pick_color(frequency: f32, gpio: GPIO) -> () {
   /*
     Note: 30 = target red freq, 108.3 = target y/r freq, 186.6 = target yellow freq, 265.0 = target yg freq
           343.2 = target green freq, 421.5 = target gb freq, 500.0 = target blue freq
@@ -131,10 +214,10 @@ fn pick_color(frequency: f32) -> () {
     }
   }
   count = count - 1;
-  select_light(count);
+  select_light(count, gpio);
 }
 
-fn select_light(val: i32) -> () {
+fn select_light(val: i32, mut gpio: GPIO) -> () {
   match val {
     0 => light_red(),
     1 => light_ry(),
@@ -142,12 +225,13 @@ fn select_light(val: i32) -> () {
     3 => light_yg(),
     4 => light_green(),
     5 => light_gb(),
-    6 => light_blue(),
+    6 => gpio.green_on(),
     _ => light_blue(),
   }
 }
 
 fn light_red() {
+/*
   r1_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   r1_gpio.set_low();
@@ -160,9 +244,11 @@ fn light_red() {
   r4_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   r4_gpio.set_low();
+*/
 }
 
 fn light_ry() {
+/*
   r1_gpio.set_high();
   y4_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
@@ -186,10 +272,11 @@ fn light_ry() {
   thread::sleep(Duration::from_millis(250));
   r4_gpio.set_low();
   r4_gpio.set_low();
-
+*/
 }
 
 fn light_yellow() {
+/*
   y1_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   y1_gpio.set_low();
@@ -202,10 +289,11 @@ fn light_yellow() {
   y4_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   y4_gpio.set_low();
-
+*/
 }
 
 fn light_yg() {
+/*
   y1_gpio.set_high();
   y4_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
@@ -229,9 +317,11 @@ fn light_yg() {
   thread::sleep(Duration::from_millis(250));
   y4_gpio.set_low();
   g1_gpio.set_low();
+*/
 }
 
 fn light_green() {
+/*
   g1_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   g1_gpio.set_low();
@@ -244,10 +334,11 @@ fn light_green() {
   g4_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   g4_gpio.set_low();
-
+*/
 }
 
 fn light_gb() {
+/*
   g1_gpio.set_high();
   b4_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
@@ -271,9 +362,11 @@ fn light_gb() {
   thread::sleep(Duration::from_millis(250));
   g4_gpio.set_low();
   b1_gpio.set_low();
+*/
 }
 
 fn light_blue() {
+/*
   b1_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   b1_gpio.set_low();
@@ -286,6 +379,7 @@ fn light_blue() {
   b4_gpio.set_high();
   thread::sleep(Duration::from_millis(250));
   b4_gpio.set_low();
+*/
 }
 
 fn find_peak(chunk: Vec<num::Complex<f32>>) -> Option<f32> {
